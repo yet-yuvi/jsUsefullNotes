f6137024ee48074ac77c8081c56d9a3f7bab91d4
jubayar <eng.jubayar@gmail.com>
2026-07-23 02:49:30 +0600

    yet-yuvi: Async/Await Pattern (ES8 / ES2017)

---
+++ promise/promiseAsyncAwait.js
@@ -0,0 +1,29 @@
+function asynchronousDivide(a, b) {
+  return new Promise((resolve, reject) => {
+    if (b === 0) {
+      reject(new Error("Can't divide by zero!!!"));
+
+      return;
+    }
+
+    console.log('It will divide eventually');
+
+    setTimeout(function () {
+      const r = a / b;
+
+      resolve(r);
+    }, 3000);
+  });
+}
+
+async function run() {
+  try {
+    const res = await asynchronousDivide(10, 5);
+
+    console.log(`Result: ${res}`);
+  } catch (err) {
+    console.warn('Failed to divide: ', err.message);
+  }
+}
+
+run();
